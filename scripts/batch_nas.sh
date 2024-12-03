006e4389199b1e994068f923087de12587ba1a34
Cranney <cranneyc@esplhpccompbio-lv01.cm.cluster>
2024-12-03 14:26:26 -0800

    HPC updates for NAS runs

--- scripts/batch_nas.sh
+++ scripts/batch_nas.sh
@@ -5,9 +5,9 @@
 #SBATCH -c 1 # number of cores
 #SBATCH --mem 79G # memory pool for all cores
 #SBATCH -t 29-23:00 # time (D-HH:MM)
-#SBATCH --job-name=gnfrmr
-#SBATCH -o /home/cranneyc/geneformer/scripts/slurmOutputs/slurm.%j.out # STDOUT
-#SBATCH -e /home/cranneyc/geneformer/scripts/slurmOutputs/slurm.%j.err # STDERR
+#SBATCH --job-name=gnfrmrNAS
+#SBATCH -o /home/cranneyc/geneformer/scripts/slurmOutputs/NAS.slurm.%j.out # STDOUT
+#SBATCH -e /home/cranneyc/geneformer/scripts/slurmOutputs/NAS.slurm.%j.err # STDERR
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=caleb.cranney@cshs.org
 
@@ -16,5 +16,5 @@ echo "geneformer start"
 eval "$(conda shell.bash hook)"
 export CUDA_LAUNCH_BLOCKING=1
 conda activate geneformer
-cd /home/cranneyc/geneformer/scripts
-python 1_train_model.py
+cd /home/cranneyc/geneformer/scripts/NAS
+python run_nas.py
