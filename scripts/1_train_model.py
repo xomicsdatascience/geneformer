0630983a73fcbf9efe38c67371e1d032b7a74880
CCranney <11773171+CCranney@users.noreply.github.com>
2024-12-13 11:05:44 -0800

    update to use wandb, shorter validation, times at checkpoints tracked

--- scripts/1_train_model.py
+++ scripts/1_train_model.py
@@ -1,6 +1,6 @@
 import torch
 import pytorch_lightning as pl
-from pytorch_lightning.loggers import TensorBoardLogger
+from pytorch_lightning.loggers import WandbLogger
 from pytorch_lightning.callbacks import ModelCheckpoint
 from geneformer import Geneformer
 from geneformer.data import GeneformerDataModule
@@ -9,6 +9,8 @@ from attention_smithy.components import MultiheadAttention, FeedForwardNetwork
 from attention_smithy.attention import StandardAttentionMethod
 from attention_smithy.utils import seed_everything
 from datasets import load_from_disk
+import time
+from datetime import timedelta
 
 class TensorBoardLoggingModelCheckpoint(ModelCheckpoint):
     def on_save_checkpoint(self, trainer, pl_module, checkpoint):
@@ -26,14 +28,11 @@ def train_model(
         dropout=0.2,
         num_layers=6,
         random_seed=0,
-        batch_size=32,
+        batch_size=128,
 ):
     seed_everything(random_seed)
 
-    logger = TensorBoardLogger(
-        "tb_logs",
-        name=f"geneformer",
-    )
+    logger = WandbLogger(project='geneformer')
 
     train_loss_checkpoint_callback = ModelCheckpoint(
         dirpath=f"checkpoints/",
@@ -53,20 +52,38 @@ def train_model(
     class ValidateAtCheckpoints(pl.Callback):
         def __init__(self, checkpoints):
             self.checkpoints = checkpoints
+            self.start_time = time.time()
+            self.last_checkpoint_time = self.start_time
+
+        def format_time(self, seconds):
+            return str(timedelta(seconds=int(seconds)))
 
         def on_train_batch_end(self, trainer, pl_module, outputs, train_batch, batch_idx, **kwargs):
             if batch_idx in self.checkpoints:
+                current_time = time.time()
+                elapsed_time_from_start = current_time - self.start_time
+                elapsed_time_from_last_checkpoint = current_time - self.last_checkpoint_time
+
+                print(f"Time elapsed from start: {self.format_time(elapsed_time_from_start)}")
+                print(f"Time elapsed from last checkpoint: {self.format_time(elapsed_time_from_last_checkpoint)}")
+
+                validation_start_time = time.time()
                 with torch.no_grad():
                     for batch in trainer.val_dataloaders:
                         pl_module.validation_step(tuple([x.to(pl_module.device) for x in batch]), batch_idx)
+                validation_end_time = time.time()
+                validation_time = validation_end_time - validation_start_time
 
+                print(f"Time spent on validation: {self.format_time(validation_time)}")
+
+                self.last_checkpoint_time = validation_end_time
     trainer = pl.Trainer(
-        max_epochs=30,
+        max_epochs=1,
         logger=logger,
         callbacks=[
             train_loss_checkpoint_callback,
             val_loss_checkpoint_callback,
-            ValidateAtCheckpoints(list(range(0, 847880, 200))[1:]),
+            ValidateAtCheckpoints(list(range(0, 847880, 1000))[1:]),
         ],
         log_every_n_steps=200,
 
